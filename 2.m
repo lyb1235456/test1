42d2219a107e051d41e0e8abcf451f26e86bbc4d
lyb1235456 <75655851+lyb1235456@users.noreply.github.com>
2020-12-08 13:12:33 +0800

    创建m文件
    第一次使用githup创建文件

---
+++ 2.m
@@ -0,0 +1,9 @@
+%%矩阵合成
+function re=comp_fu(A,B)
+    for i=1:size(A,1)   %%%模糊矩阵合成算法 A.B
+        for j=1:size(B,2)
+            AB(i,j)=max(min(A(i,:),B(:,j)'));
+        end
+    end
+    re=AB;
+end
